466b649615aec9bf73d5ab373835b03f5ce68bb2
Jose Padilla Méndez <jpadilla@lpuig.xeill.net>
2025-04-10 16:49:08 +0200

    Trabajo en zona2

--- includes/templates/calltoaction2.php
+++ includes/templates/calltoaction2.php
@@ -5,9 +5,9 @@
         <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
           <div class="col-xl-10">
             <div class="text-center">
-              <h3>Call To Action</h3>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <a class="cta-btn" href="#">Call To Action</a>
+              <h3>Llamado a la acción</h3>
+              <p>Pero el dolor de los que abandonan sus deberes, es decir, el del trabajo, es el que los ciega, pues ellos mismos abandonan sus deberes.</p>
+              <a class="cta-btn" href="#">Llamado a la acción</a>
             </div>
           </div>
         </div>
